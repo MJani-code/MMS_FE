86b1dc2330a816f5bea6aa43b9e86ee47d83bffc
Jani <martonjanos1990@gmail.com>
2025-04-25 13:18:28 +0200

    Update/align the structure

--- components/pages/tasks/RepairReports.vue
+++ components/pages/tasks/RepairReports.vue
@@ -1,10 +1,14 @@
 <template>
   <v-container class="ma-2" style="padding: unset !important">
     <v-expansion-panels v-model="panel" multiple>
-      <v-expansion-panel class="accordion">
+      <v-expansion-panel
+        v-for="intervention in interventions"
+        v-bind:key="intervention.interventionId"
+        class="accordion"
+      >
         <v-expansion-panel-header style="border-bottom: #f07b00 5px solid">
-          Beavatkozás
-          <span class="text-right ml-2">Timestamp</span>
+          {{ intervention.name }}
+          <span class="text-right ml-2">{{ intervention.timestamp }}</span>
           <span class="text-right ml-2">
             <v-icon class="mdi mdi-trash-can"></v-icon>
           </span>
@@ -12,24 +16,25 @@
         <v-expansion-panel-content class="pt-2">
           <v-row align="center">
             <v-card-subtitle>Hibák:</v-card-subtitle>
-            <v-chip>Hiba 1</v-chip>
-            <v-chip>Hiba 2</v-chip>
+            <v-chip v-for="(issue, index) in intervention.issues" :key="index">
+              {{ issue.name }}
+            </v-chip>
           </v-row>
           <v-row align="center" class="pt-2">
             <v-card-subtitle>Felhasznált alkatrészek:</v-card-subtitle>
-            <v-chip>Alkatrész 1</v-chip>
-            <v-chip>Alkatrész 1</v-chip>
-            <v-chip>Alkatrész 1</v-chip>
-            <v-chip>Alkatrész 1</v-chip>
+            <v-chip v-for="(part, index) in intervention.parts" :key="index">
+              {{ part.name }} ({{ part.quantity }})
+            </v-chip>
           </v-row>
           <v-row align="center" class="pt-4">
-            <v-textarea
-              v-model="repairReport"
-              auto-grow
-              label="Javítás leírása"
-              outlined
-              disabled
-            ></v-textarea>
+            <v-col cols="12">
+              <v-textarea
+                v-model="intervention.notes"
+                label="Leírás"
+                outlined
+                disabled
+              ></v-textarea>
+            </v-col>
           </v-row>
         </v-expansion-panel-content>
       </v-expansion-panel>
@@ -43,7 +48,26 @@ export default {
     panel: [],
     selectedIssues: ['Hiba 1', 'Hiba 2'],
     replacedSpareparts: ['Alkatrész 1', 'Alkatrész 2'],
-    repairReport: 'Javítás leírása'
+    repairReport: 'Javítás leírása',
+    interventions: [
+      {
+        name: 'Alkatrészcsere',
+        uuid: 'EXOHU-AR-E-000010',
+        issues: [
+          {
+            name: 'LID NOT OPENING'
+          }
+        ],
+        parts: [
+          {
+            name: 'Part1',
+            quantity: 1
+          }
+        ],
+        notes: 'Megjavítva, minden rendben működik',
+        timestamp: '2024-10-01 12:00'
+      }
+    ]
   })
 };
 </script>
