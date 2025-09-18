d8984ebdf76da0206ce6ee55c290275d107c3a3b
Jani <martonjanos1990@gmail.com>
2025-09-18 12:47:48 +0200

    version update to 1.0.1

--- layouts/default.vue
+++ layouts/default.vue
@@ -63,7 +63,7 @@
     <v-footer :absolute="!fixed" app>
       <span
         >&copy; {{ new Date().getFullYear() }} BY: Martolin. Version:
-        1.0.0</span
+        1.0.1</span
       >
     </v-footer>
   </v-app>
