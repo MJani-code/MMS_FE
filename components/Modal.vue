4c13c6d89b4a5662d4d74ba67efa461e434f59b7
Jani <martonjanos1990@gmail.com>
2026-07-23 15:11:21 +0200

    fix(ui): wrap long notification text in modal title

--- components/Modal.vue
+++ components/Modal.vue
@@ -6,7 +6,7 @@
       max-width="290"
     >
       <v-card>
-        <v-card-title class="text-h5">
+        <v-card-title class="text-h5 text-break">
           <p>{{ $store.state.notification.modal.message }}</p>
         </v-card-title>
         <!-- <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text> -->
@@ -39,3 +39,9 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.text-break {
+  word-break: break-word;
+}
+</style>
